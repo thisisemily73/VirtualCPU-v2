dcb7c12dbedaf92d2776c4cd05b84320311644d0
Emily <whynotbemily@gmail.com>
2026-07-16 22:09:21 -0400

    Updated assembler.java to finish encoding

--- src/assembler/Assembler.java
+++ src/assembler/Assembler.java
@@ -11,29 +11,37 @@ import java.util.*;
  * followed by the parser, before becoming legible
  * instruction objects.
  * 
- * The output of this stage will later be converted
- * into binary machine code.
+ * The output of this stage is a 16-bit machine code
+ * compatible with the CPU's memory system.
  */
 
 public class Assembler {
     
     private Tokenizer tokenizer;
     private Parser parser;
+    private InstructionEncoder encoder;
 
     public Assembler() {
         tokenizer = new Tokenizer();
         parser = new Parser();
+        encoder = new InstructionEncoder();
     }
 
-    // Converts the assembly text into a list of instructions.
+    // Converts assembly text into 16-bit machine code.
 
-    public List<Instruction> assemble(String program) {
+    public List<Integer> assemble(String program) {
         // Break source code into tokens
         List<String[]> tokens = tokenizer.tokenize(program);
         
         // Convert tokens into instructions
         List<Instruction> instructions = parser.parse(tokens);
 
-        return instructions;
+        // Encode instructions into binary machine code
+        List<Integer> machineCode = new ArrayList<>();
+        for (Instruction instruction : instructions) {
+            machineCode.add(encoder.encode(instruction));
+        }
+
+        return machineCode;
     }
 }
